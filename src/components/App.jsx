8e903ae4fdeb71c58b95e7862b09cbe5dd99303c
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-09-12 13:12:40 -0700

    Use grid and container componenets to organize card layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,49 @@
 import ItemCard from './Card'
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import { useState, useEffect } from 'react'
 import '../styles/App.css'
 
 function App() {
+  const [spriteList, setSpriteList] = useState([]);
+
+  async function getSprites(){
+    //Get pokedex and extract list of pokemon
+    try{
+      const rawData = await fetch("https://pokeapi.co/api/v2/pokedex/kanto/");
+      const pokedex = await rawData.json();
+      const pokeList = pokedex.pokemon_entries;
+      //Get corresponding sprite for each pokemon
+      const sprites = await Promise.all(
+        pokeList.map(async (item) => {
+          try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${item.pokemon_species.name}/`);
+            const pokemon = await response.json();
+            return pokemon.sprites.front_default;
+          } catch (error) {
+            console.error("Error:", error);
+            return null; 
+          }
+        })
+      );
+      setSpriteList(sprites);
+    }
+    catch(error){
+      console.log("Error:", error);
+    }
+  }
+
+  useEffect(() => {
+    getSprites();
+  },[]);
+
   return (
     <>
-      <ItemCard></ItemCard>
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+          {spriteList.map((item, index) => <Grid key={index} xs={3}><ItemCard img={item}></ItemCard></Grid>)}
+        </Grid>
+      </Container>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { Card } from "@mui/material";
+import PropTypes from 'prop-types';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -9,12 +10,12 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
 
 
-function ItemCard(){
+function ItemCard({img}){
     return(
         <Card sx={{maxWidth: 300}}>
             <CardMedia
                 sx={{ height: 100 }}
-                image="https://picsum.photos/id/237/200/300"
+                image={img}
                 title="place holder"
             />
             <CardContent sx={{maxHeight: 100, overflow: "scroll"}}>
@@ -22,7 +23,7 @@ function ItemCard(){
                     Product Name
                 </Typography>
                 <Typography variant="body1" component='p'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </Typography>   
             </CardContent>
             <CardActions sx={{display:"flex", justifyContent:"space-between"}}>
@@ -36,5 +37,9 @@ function ItemCard(){
         </Card>
     )
 }
+
+ItemCard.propTypes = {
+    img: PropTypes.string.isRequired,
+};
 
 export default ItemCard;
