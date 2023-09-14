6f0386dfd5ad5a5539fb17f9675450938d1f99ae
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-09-13 20:05:56 -0700

    Nav bar now routes to home page and shop page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,23 +4,30 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <HomeIcon />
-          </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Shop
-        </Typography>
+        <Link to='/' style={{textDecoration: 'none', color:'white'}}>
+          <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <HomeIcon />
+            </IconButton>
+        </Link>
+        <div style={{flexGrow: 1}}>
+        <Link to='/shop' style={{textDecoration: 'none', color: 'white'}}>
+          <Typography variant="h6" component="div">
+              Shop
+          </Typography>
+        </Link>
+        </div>
         <ShoppingCartIcon/>
       </Toolbar>
     </AppBar>
